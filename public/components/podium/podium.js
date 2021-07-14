d338c015dbdd3cb7d48a0e0159454dd84648856f
Jens Keller <jens.keller@sap.com>
2021-07-14 08:34:42 +0800

    simplify podium handling

--- public/components/podium/podium.js
+++ public/components/podium/podium.js
@@ -2,27 +2,12 @@
 
 function Podium(props) {
 
-    const getImg = (player) => {
-        return ['🥇', '🥈', '🥉'][player.rank - 1] || ''
-        // const img = ['GoldCup.svg', 'SilverCup.svg', 'BronzeCup.svg']
-        // const imgSrc = img[player.rank - 1] ? `components/podium/${img[player.rank - 1]}` : undefined
-        // return imgSrc ? <img style={{ width: "10%" }} src={imgSrc}/> : ''
-    }
+    const html = props.players.map((p) => {
+        const img = ['🥇', '🥈', '🥉'][p.rank - 1] || ''
+        return <div key={p.name} className="podium">
+            {img} {p.avatar} {p.name}
+        </div>
+    })
 
-    const getPlayer = (i) => props.players[i] || { avatar: '' }
-    // REVISE make it completely dynamic instead of hard coding 4
-    return <div style={{ width: "100%", display: "flex", flexDirection: "column" }}>
-        <div className="podiumFirst" style={{ fontSize: "5em", color: "white", fontFamily: "komika_textregular", padding: "2%", textAlign: "center" }}>
-            {getImg(getPlayer(0))} {getPlayer(0).avatar} {getPlayer(0).name}
-        </div>
-        <div className="podiumSecond" style={{ fontSize: "5em", color: "white", fontFamily: "komika_textregular", padding: "2%", textAlign: "center" }}>
-            {getImg(getPlayer(1))} {getPlayer(1).avatar} {getPlayer(1).name}
-        </div>
-        <div className="podiumThird" style={{ fontSize: "5em", color: "white", fontFamily: "komika_textregular", padding: "2%", textAlign: "center" }}>
-            {getImg(getPlayer(2))} {getPlayer(2).avatar} {getPlayer(2).name}
-        </div>
-        <div className="podiumFourth" style={{ fontSize: "5em", color: "white", fontFamily: "komika_textregular", padding: "2%", textAlign: "center" }}>
-            {getImg(getPlayer(3))} {getPlayer(3).avatar} {getPlayer(3).name}
-        </div>
-    </div>
+    return <div>{html}</div>
 }
