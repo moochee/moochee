a05c55659bf502b4a9812b3aed9c563cc5509e52
Yang Zhao <yang.zhao@sap.com>
2021-07-13 19:47:05 -0700

    fix: questionsWithoutRightAnswer is shared among games

--- ws-server-adapter.js
+++ ws-server-adapter.js
@@ -5,17 +5,15 @@ export default function WsServerAdapter(setTimeout, questions) {
   let avatars = Array.from('🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐸🐵🐔🐧🐤🦉🐴🦄🐝🐛🦋🐌🐞🐜🦂🐢🐍🦎🦖🐙🦀🐠🐬🐳🦈🦭🐊🦧🦍🦣🐘🦏🐫🦒🦬🐿🦔🦡🐲')
   const games = []
 
-  // REVISE just checking - why do we need to map the whole thing, can't we just do it on the fly in "nextRound"
-  const questionsWithoutRightAnswer = questions.map((q, index) => {
-    return { sequence: index + 1, text: q.text, answers: q.answers }
-  })
-
   this.host = () => {
     const gameId = String(nextGameId++)
+    const remainingQuestions = questions.map((q, index) => {
+      return { sequence: index + 1, text: q.text, answers: q.answers }
+    })
     const questionsAndGuesses = questions.map(q => {
         return { text: q.text, rightAnswer: q.rightAnswer, guesses: [] }
     })
-    games.push({ id: gameId, questions: questionsAndGuesses, players: [] })
+    games.push({ id: gameId, remainingQuestions, questionsAndGuesses, players: [] })
     return gameId
   }
 
@@ -27,7 +25,8 @@ export default function WsServerAdapter(setTimeout, questions) {
   }
 
   this.nextRound = (gameId) => {
-    const question = questionsWithoutRightAnswer.shift()
+    const game = games.find(g => g.id === gameId)
+    const question = game.remainingQuestions.shift()
     const timeToGuess = 20000;
     setTimeout(() => finishRound(gameId), timeToGuess)
     return { question, timeToGuess }
@@ -35,8 +34,7 @@ export default function WsServerAdapter(setTimeout, questions) {
 
   this.guess = (gameId, questionText, playerName, answer) => {
     const game = games.find(g => g.id === gameId)
-
-    const question = game.questions.find(q => q.text === questionText)
+    const question = game.questionsAndGuesses.find(q => q.text === questionText)
     //TODO: make sure only 1 answer per player
     question.guesses.push({playerName, answer})
 
@@ -46,7 +44,7 @@ export default function WsServerAdapter(setTimeout, questions) {
     if (question.guesses.length === game.players.length) {
       return finishRound(gameId)
     } else {
-      return {event: 'roundInProgress', result: null}
+      return {event: '', result: null}
     }
   }
 
@@ -55,7 +53,7 @@ export default function WsServerAdapter(setTimeout, questions) {
     // TODO implement "player is on fire", e.g. when climbed 3 times, or guessed right 3 times, or ...
     const result = [...game.players]
     result.sort((a, b) => b.score - a.score)
-    if (questionsWithoutRightAnswer.length > 0) {
+    if (game.remainingQuestions.length > 0) {
       return {event: 'roundFinished', result}
     } else {
       return {event: 'gameFinished', result}
