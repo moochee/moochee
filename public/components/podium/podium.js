9473e10b4ee882c9588f53a4063ecb4a0e7dc7e5
Jens Keller <jens.keller@sap.com>
2021-07-25 17:59:08 +0800

    podium dynamic bind players

--- public/components/podium/podium.js
+++ public/components/podium/podium.js
@@ -6,15 +6,15 @@ function Podium(props) {
     //     document.getElementById('fireworks').play()
     // }, 14000)
 
-    // const playerToRankHtml = (player, rank) => {
-    //     return <div className={`podiumRank${rank}`}>
-    //         <div>{player.name}</div>
-    //         <div className={`podiumAvatar${rank}`}>{player.avatar}</div>
-    //     </div>
-    // }
-    // const players = props.players || []
+    const playerToRankHtml = (player, rank) => {
+        return <div key={player.name} className={`podiumRank${rank}`}>
+            <div>{player.name}</div>
+            <div className={`podiumAvatar${rank}`}>{player.avatar}</div>
+        </div>
+    }
+    const first3Players = props.players.slice(0, 3)
 
-    // players.map((p, index) => playerToRankHtml(p, index + 1))
+    const podiumPlayersHtml = first3Players.map((p, index) => playerToRankHtml(p, index + 1))
 
     return <div className="podium">
         <audio id="music" src="components/podium/arcade-game-music-loop.mp3"></audio>
@@ -23,19 +23,6 @@ function Podium(props) {
         <img className="firework firework2" src="components/podium/celebration-152950.svg" />
         <img className="firework firework3" src="components/podium/celebration-152951.svg" />
 
-        <div className="podiumRank1">
-            <div>Alice</div>
-            <div className="podiumAvatar1">🐰</div>
-        </div>
-
-        <div className="podiumRank2">
-            <div>Bob Marley</div>
-            <div className="podiumAvatar2">🦁</div>
-        </div>
-
-        <div className="podiumRank3">
-            <div>Cindy</div>
-            <div className="podiumAvatar3">🐵</div>
-        </div>
+        {podiumPlayersHtml}
     </div>
 }
