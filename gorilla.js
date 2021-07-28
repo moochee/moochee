c1fa87787ee165ca2a6941d67703100d74b0a58a
Jens Keller <jens.keller@sap.com>
2021-07-28 22:21:38 +0800

    separate imports from logic/commands

--- gorilla.js
+++ gorilla.js
@@ -1,56 +1,57 @@
 'use strict'
 
 import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import EventEmitter from './event-emitter.js'
+import QuizRepo from './quiz-repo.js'
+import Games from './games.js'
+
 const app = express()
 app.use(express.static('public'))
 
-import http from 'http'
 const server = http.createServer(app)
 
-import { Server } from 'socket.io'
 const io = new Server(server)
-import EventEmitter from './event-emitter.js'
 const eventEmitter = new EventEmitter(io)
-import QuizRepo from './quiz-repo.js'
 const quizRepo = new QuizRepo()
-import Games from './games.js'
 const games = new Games(setTimeout, quizRepo, eventEmitter)
 
 io.on('connection', (socket) => {
-  socket.on('getQuizzes', async (callback) => {
-    const quizzes = await quizRepo.getAll()
-    callback(quizzes)
-  })
+    socket.on('getQuizzes', async (callback) => {
+        const quizzes = await quizRepo.getAll()
+        callback(quizzes)
+    })
 
-  socket.on('host', async (quizId, callback) => {
-    const gameId = await games.host(quizId)
-    socket.join(gameId)
-    callback(gameId)
-  })
-  
-  socket.on('join', (gameId, name, callback) => {
-    try {
-      games.join(gameId, name, socket.id)
-      socket.join(gameId)
-      callback()
-    } catch (error) {
-      callback(error.message)
-    }
-  })
-  
-  socket.on('nextRound', (gameId) => {
-    games.nextRound(gameId)
-  })
+    socket.on('host', async (quizId, callback) => {
+        const gameId = await games.host(quizId)
+        socket.join(gameId)
+        callback(gameId)
+    })
 
-  socket.on('guess', (gameId, questionText, playerName, answer) => {
-    games.guess(gameId, questionText, playerName, answer)
-  })
+    socket.on('join', (gameId, name, callback) => {
+        try {
+            games.join(gameId, name, socket.id)
+            socket.join(gameId)
+            callback()
+        } catch (error) {
+            callback(error.message)
+        }
+    })
 
-  socket.on('disconnect', () => {
-    games.disconnect(socket.id)
-  })
+    socket.on('nextRound', (gameId) => {
+        games.nextRound(gameId)
+    })
+
+    socket.on('guess', (gameId, questionText, playerName, answer) => {
+        games.guess(gameId, questionText, playerName, answer)
+    })
+
+    socket.on('disconnect', () => {
+        games.disconnect(socket.id)
+    })
 })
 
 const listener = server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started on *:${listener.address().port}`)
+    console.log(`Server started on *:${listener.address().port}`)
 })
