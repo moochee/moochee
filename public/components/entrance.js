17d787c27c6fe4976c0238546d93b3b955d27eee
Jens Keller <jens.keller@sap.com>
2021-08-03 11:59:53 +0800

    styling for entrance

--- public/components/entrance.js
+++ public/components/entrance.js
@@ -1,46 +1,31 @@
-"use strict";
+'use strict'
 
 function Entrance(props) {
-    const [quizzes, setQuizzes] = React.useState([]);
+    const [quizzes, setQuizzes] = React.useState([])
+
+    const colors = ['green', 'blue', 'orange', 'purple']
 
     React.useEffect(async () => {
-        //const list = await props.adapter.getQuizzes();
-        const list = [
-            { id: 1, text: '1'},
-            { id: 2, text: '1'},
-            { id: 3, text: '1'},
-            { id: 4, text: '1'},
-            { id: 5, text: '1'},
-            { id: 6, text: '1'},
-            { id: 7, text: '1'}
-        ]
-        setQuizzes(list);
-    }, []);
+        const list = await props.adapter.getQuizzes()
+        list.forEach((entry, index) => entry.color = colors[index % 4])
+        setQuizzes(list)
+    }, [])
 
     const host = async (quizId) => {
-        const gameId = await props.adapter.host(quizId);
-        props.onHost(gameId);
-    };
+        const gameId = await props.adapter.host(quizId)
+        props.onHost(gameId)
+    }
 
     const quizList = quizzes.map((q) => {
-        return (
-            <StickyCard
-                key={q.id}
-                onClick={() => host(q.id)}
-                text={q.text}
-                color="blue"
-            />
-        );
-    });
+        return <StickyCard key={q.id} onClick={() => host(q.id)} text={q.text} color={q.color} />
+    })
 
-    return (
-        <div style={{ display: "flex", flexDirection: "column" }}>
-            <h1>Welcome to the Gorilla Quiz App!</h1>
-            <h4>Select one quiz below to host a new game</h4>
-            <p />
-            <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-                {quizList}
-            </div>
+    return <div style={{ fontFamily: 'komika_textregular', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+        <h1>Welcome to the Gorilla Quiz App!</h1>
+        <h4>Select one quiz below to host a new game</h4>
+        <p />
+        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            {quizList}
         </div>
-    );
+    </div>
 }
--- public/components/sticky-card/sticky-card.js
+++ public/components/sticky-card/sticky-card.js
@@ -5,12 +5,11 @@ function StickyCard(props) {
 
 	const click = () => {
 		setStickyClass('stickyCard stickyThrowAway')
-		props.onClick()
 	}
 
 	const img = `components/sticky/sticky-${props.color}.svg`
 
-	return <div onClick={click} className={stickyClass} >
+	return <div onAnimationEnd={props.onClick} onClick={click} className={stickyClass} >
 		<img src={img}></img>
 		<div className="textField">
 			{props.text}
