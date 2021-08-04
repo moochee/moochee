6a5bce775c7540608ee1ec643f239b6e0c2905f0
Jens Keller <jens.keller@sap.com>
2021-08-04 10:09:01 +0800

    shell incl demo page

---
+++ public/components/shell/shell.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function Shell(props) {
+    return <div className='shell'>
+        <div className='shell-header'>
+            {props.header}
+        </div>
+        <div className='shell-content'>
+            {props.children}
+        </div>
+        <div className='shell-footer'>
+            <div className='shell-footer-left'></div>
+            <div className='shell-footer-right'></div>
+        </div>
+    </div>
+}
