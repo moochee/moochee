92a2deab0ab17747410ad8aaa016a0c9d85aba39
Yang Zhao <yang.zhao@sap.com>
2021-08-11 15:56:18 -0700

    merge entranceweb/hostgameweb and joingameweb/playgameweb

--- public/components/web-app.js
+++ public/components/web-app.js
@@ -1,54 +1,36 @@
 'use strict'
 
-// REVISE merge those things together, cause anyway there is state in the transition
-Gorilla.EntranceWeb = function (props) {
-    const history = ReactRouterDOM.useHistory()
-    const host = (gameId, quizTitle) => {
-        props.onHost(quizTitle)
-        history.push(`host/${gameId}`)
-    }
-    return <Gorilla.Entrance adapter={props.adapter} onHost={host} />
-}
-
 Gorilla.HostGameWeb = function (props) {
-    const { gameId } = ReactRouterDOM.useParams()
     const history = ReactRouterDOM.useHistory()
+    const [atEntrance, setAtEntrance] = React.useState(true)
+    const [gameId, setGameId] = React.useState('')
+    const [quizTitle, setQuizTitle] = React.useState('')
+
+    const host = (gameId, quizTitle) => {
+        setGameId(gameId)
+        setQuizTitle(quizTitle)
+        setAtEntrance(false)
+    }
+
     // REVISE check if can find a better name for onReplay - basically it is sending the host back to the home page to be able to host another quiz if he/she wants to
-    return <div style={{ height: '100%' }}>
-        <Gorilla.HostGame gameId={gameId} adapter={props.adapter} quizTitle={props.quizTitle} onReplay={() => history.push('/')} />
-    </div>
+    return atEntrance ?
+        <Gorilla.Entrance adapter={props.adapter} onHost={host} /> :
+        <Gorilla.HostGame gameId={gameId} adapter={props.adapter} quizTitle={quizTitle} onReplay={() => history.push('/')} />
 }
 
 Gorilla.PlayGameWeb = function (props) {
-    const { gameId, playerName } = ReactRouterDOM.useParams()
-    return <div style={{ height: '100%' }}>
-        <Gorilla.PlayGame gameId={gameId} adapter={props.adapter} playerName={playerName} playerAvatar={props.avatar} otherPlayers={props.otherPlayers} onPlayerJoined={props.onPlayerJoined} onPlayerDisconnected={props.onPlayerDisconnected} />
-    </div>
-}
-
-Gorilla.JoinGameWeb = function (props) {
     const { gameId } = ReactRouterDOM.useParams()
-    const history = ReactRouterDOM.useHistory()
-
-    const join = (playerName, avatar, otherPlayers) => {
-        history.push(`${gameId}/${playerName}`)
-        props.onJoin(avatar, otherPlayers)
-    }
-
-    return <Gorilla.JoinGame gameId={gameId} adapter={props.adapter} onJoin={join} />
-}
-
-Gorilla.WebApp = function (props) {
-    const { HashRouter, Switch, Route } = ReactRouterDOM
-    const [avatar, setAvatar] = React.useState('')
+    const [atJoinGame, setAtJoinGame] = React.useState(true)
+    const [playerName, setPlayerName] = React.useState('')
+    const [playerAvatar, setPlayerAvatar] = React.useState('')
     const [otherPlayers, setOtherPlayers] = React.useState([])
-    const [quizTitle, setQuizTitle] = React.useState('')
 
-    const join = (avatar, otherPlayers) => {
-        setAvatar(avatar)
+    const join = (playerName, playerAvatar, otherPlayers) => {
+        setAtJoinGame(false)
+        setPlayerName(playerName)
+        setPlayerAvatar(playerAvatar)
         setOtherPlayers(otherPlayers)
     }
-
     const addPlayer = (otherPlayer) => {
         setOtherPlayers((oldOtherPlayers) => [...oldOtherPlayers, otherPlayer])
     }
@@ -56,23 +38,24 @@ Gorilla.WebApp = function (props) {
     const removePlayer = (player) => {
         setOtherPlayers((oldPlayers) => oldPlayers.filter(p => p != player))
     }
+    return atJoinGame ?
+        <Gorilla.JoinGame gameId={gameId} adapter={props.adapter} onJoin={join} /> :
+        <Gorilla.PlayGame gameId={gameId} adapter={props.adapter}
+            playerName={playerName} playerAvatar={playerAvatar} otherPlayers={otherPlayers}
+            onPlayerJoined={addPlayer} onPlayerDisconnected={removePlayer} />
+}
 
-    // REVISE merge entrance/entranceweb with hostgame/hostgameweb and joingame/joingameweb with playgame/playgameweb
-    //        by this, we won't need the title and avatar bubbling all the way up and down any more
+Gorilla.WebApp = function (props) {
+    const { HashRouter, Switch, Route } = ReactRouterDOM
+
     return <div style={{ height: '100%', width: '100%' }}>
         <HashRouter>
             <Switch>
                 <Route exact path='/'>
-                    <Gorilla.EntranceWeb adapter={props.adapter} onHost={setQuizTitle} />
-                </Route>
-                <Route path='/play/:gameId/:playerName'>
-                    <Gorilla.PlayGameWeb adapter={props.adapter} avatar={avatar} otherPlayers={otherPlayers} onPlayerJoined={addPlayer} onPlayerDisconnected={removePlayer} />
+                    <Gorilla.HostGameWeb adapter={props.adapter} />
                 </Route>
                 <Route path='/play/:gameId'>
-                    <Gorilla.JoinGameWeb adapter={props.adapter} onJoin={join} />
-                </Route>
-                <Route path='/host/:gameId'>
-                    <Gorilla.HostGameWeb adapter={props.adapter} quizTitle={quizTitle} />
+                    <Gorilla.PlayGameWeb adapter={props.adapter} />
                 </Route>
             </Switch>
         </HashRouter>
