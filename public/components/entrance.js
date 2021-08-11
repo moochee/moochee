80a8903e8cc1680c5ce3bc63ff41cebcb44e4b88
Jens Keller <jens.keller@sap.com>
2021-08-11 11:01:07 +0800

    add shell to entry pages for host and players

--- public/components/entrance.js
+++ public/components/entrance.js
@@ -20,12 +20,13 @@ Gorilla.Entrance = function (props) {
         return <Gorilla.StickyButton key={q.id} onClick={() => host(q.id, q.text)} text={q.text} color={q.color} />
     })
 
-    return <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
-        <h1>Welcome to the 🦍 Quiz App!</h1>
-        <h2>Select a quiz below to host a new game</h2>
-        <p />
-        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-            {quizList}
+    return <Gorilla.Shell headerCenter='Welcome to the 🦍 Quiz App!'>
+        <div style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
+            <h1>Select a quiz below to host a new game</h1>
+            <p />
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                {quizList}
+            </div>
         </div>
-    </div>
+    </Gorilla.Shell>
 }
--- public/components/join-game.js
+++ public/components/join-game.js
@@ -14,12 +14,14 @@ Gorilla.JoinGame = function (props) {
         }
     }
 
-    return <div style={{ display: 'flex', flexDirection: 'column', height: '80%', alignItems: 'center', justifyContent: 'center' }}>
-        <h1>Game {props.gameId}</h1>
-        <input style={{ width: '10em', height: '3em', textAlign: 'center', fontFamily: 'inherit', fontSize: 'inherit' }}
-            id='playerName' placeholder='Enter your name' autoFocus={true} value={playerName}
-            onInput={updatePlayerName} onKeyPress={e => e.code === 'Enter' ? join() : null}></input>
-        <button style={{ width: '10.5em', height: '3em', fontFamily: 'inherit', fontSize: 'inherit' }} onClick={join}>Join</button>
-        <div>{errorMessage}</div>
-    </div>
+    return <Gorilla.Shell headerCenter='Welcome to the 🦍 Quiz App!'>
+        <div style={{ display: 'flex', flexDirection: 'column', height: '80%', alignItems: 'center', justifyContent: 'center' }}>
+            <h1>Join Game {props.gameId}</h1>
+            <input style={{ width: '10em', height: '3em', textAlign: 'center', fontFamily: 'inherit', fontSize: 'inherit' }}
+                id='playerName' placeholder='Enter your name' autoFocus={true} value={playerName}
+                onInput={updatePlayerName} onKeyPress={e => e.code === 'Enter' ? join() : null}></input>
+            <button style={{ width: '10.5em', height: '3em', fontFamily: 'inherit', fontSize: 'inherit' }} onClick={join}>Join</button>
+            <div>{errorMessage}</div>
+        </div>
+    </Gorilla.Shell>
 }
