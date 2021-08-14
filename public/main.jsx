3765a74a701bda02dd7ef2a3679122de0d45df9a
Jens Keller <jens.keller@sap.com>
2021-08-14 15:35:55 +0800

    service worker: include caching + cleanup

--- public/main.jsx
+++ public/main.jsx
@@ -3,6 +3,6 @@
 // this file could become our bootstrapping file:
 // load the initial components like web-app.js using 'import', and rendering them using jsx
 
-import QuizSocketClient from '../quiz-socket-client.js'
+import QuizSocketClient from './quiz-socket-client.js'
 
 ReactDOM.render(<div>hello world</div>, document.querySelector('#react-root'))
--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,41 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', () => {
     self.importScripts('./lib/babel.min.js')
 })
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(self.clients.claim())
-    console.log('activated')
 })
 
 self.addEventListener('fetch', (event) => {
-    console.log(event.request)
-    // REVISE below if is of course prototypical - I think we should consider
-    //        use the serviceworker scope on registration, e.g. only consider /components
-    //        filter for file suffix - everything else than .jsx just gets plain-fetched
-    if (event.request.url.indexOf('main.js') > -1) {
-        console.log('compile & serve')
-        event.respondWith(fetch(event.request).then(res => res.text()).then(text => {
-            const compiledCode = Babel.transform(text, { presets: ['react'] }).code
-            return new Response(compiledCode, {
-                status: 200,
-                statusText: 'OK',
-                headers: {'Content-Type': 'application/javascript'}
-            })
-        }))
-    } else {
-        console.log('just serve')
-        event.respondWith(fetch(event.request))
+    const intercept = async () => {
+        const cache = await caches.open('static-v1')
+        const cacheResponse = await cache.match(event.request)
+        return cacheResponse || fetchAndUpdateCacheIfOnline(event.request, cache).catch(console.error)
+    }
+
+    // filter funny stuff like Chrome extensions etc.
+    // TODO check if service worker even considers foreign origins, e.g. if we load stuff from unpkg, and if so, if we want to also consider these (I think yes)
+    // TODO check that we don't intercept API requests, hopefully it's fine since we're using web sockets, but since service worker seems to also intercept other protocols like chrome://, we better double-check...
+    if (['http:', 'https:'].includes(new URL(event.request.url).protocol)) {
+        event.respondWith(intercept())
     }
 })
+
+const jsxToJs = async (resp) => {
+    const text = await resp.text()
+    // eslint-disable-next-line no-undef
+    const compiledCode = Babel.transform(text, { presets: ['react'] }).code
+    const headers = new Headers(resp.headers)
+    headers.set('Content-Type', 'application/javascript; charset=UTF-8')
+    return new Response(compiledCode, { status: 200, statusText: 'OK', headers })
+}
+
+const fetchAndUpdateCacheIfOnline = async (request, cache) => {
+    if (navigator.onLine) {
+        let resp = await fetch(request)
+        resp = (resp.headers.get('content-type').indexOf('text/jsx') > -1) ? await jsxToJs(resp) : resp
+        // TODO update cache only if status = 2XX
+        await cache.put(request, resp.clone())
+        return resp
+    }
+}
