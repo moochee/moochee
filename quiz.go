e9f9ceaf0c1a2cfba6341999ce73654e2dfe9733
katie yang <katie.yang@sap.com>
2021-08-19 13:37:44 +0800

    switch to next question

--- quiz.go
+++ quiz.go
@@ -11,6 +11,7 @@ type Quiz struct {
 	// REVISE try a map instead of an array
 	playerList                    []Player
 	numberOfplayersWhoHaveGuessed int
+	allQuestions                  [7]QuizQuestion
 }
 
 type Player struct {
@@ -19,64 +20,65 @@ type Player struct {
 }
 
 func (q *Quiz) Start() {
-	allQuestions := [7]QuizQuestion{}
+	q.allQuestions = [7]QuizQuestion{}
 
-	allQuestions[0] = QuizQuestion{}
-	allQuestions[0].question = "Are you in?"
-	allQuestions[0].answers[0] = "Yes"
-	allQuestions[0].answers[1] = "No"
-	allQuestions[0].answers[2] = "Maybe"
-	allQuestions[0].answers[3] = "I don't know"
-	allQuestions[0].rightAnswer = "Yes"
+	q.allQuestions[0] = QuizQuestion{}
+	q.allQuestions[0].question = "Are you in?"
+	q.allQuestions[0].answers[0] = "Yes"
+	q.allQuestions[0].answers[1] = "No"
+	q.allQuestions[0].answers[2] = "Maybe"
+	q.allQuestions[0].answers[3] = "I don't know"
+	q.allQuestions[0].rightAnswer = "Yes"
 	// q.allQuestions[0].question = "..." - before we write like it, but we delete q. as allQuestions doesn't need to belong quize, and we also want to save memory
 
-	allQuestions[1] = QuizQuestion{}
-	allQuestions[1].question = "What does PPO stand for?"
-	allQuestions[1].answers[0] = "Product Passionate Ownership"
-	allQuestions[1].answers[2] = "Private Product Ownership"
-	allQuestions[1].answers[1] = "Passionate Product Ownership"
-	allQuestions[1].answers[3] = "Potential Product Optimization"
-	allQuestions[1].rightAnswer = "Passionate Product Ownership"
+	q.allQuestions[1] = QuizQuestion{}
+	q.allQuestions[1].question = "What does PPO stand for?"
+	q.allQuestions[1].answers[0] = "Product Passionate Ownership"
+	q.allQuestions[1].answers[2] = "Private Product Ownership"
+	q.allQuestions[1].answers[1] = "Passionate Product Ownership"
+	q.allQuestions[1].answers[3] = "Potential Product Optimization"
+	q.allQuestions[1].rightAnswer = "Passionate Product Ownership"
 
-	allQuestions[2] = QuizQuestion{}
-	allQuestions[2].question = "What is the fundamental principle of PPO?"
-	allQuestions[2].answers[0] = "Minimize Output - Minimize Outcome"
-	allQuestions[2].answers[1] = "Maximize Output - Minimize Outcome"
-	allQuestions[2].answers[2] = "Minimize Output - Maximize Outcome"
-	allQuestions[2].answers[3] = "Maximize Output - Maximize Outcome"
-	allQuestions[2].rightAnswer = "Minimize Output - Maximize Outcome"
+	q.allQuestions[2] = QuizQuestion{}
+	q.allQuestions[2].question = "What is the fundamental principle of PPO?"
+	q.allQuestions[2].answers[0] = "Minimize Output - Minimize Outcome"
+	q.allQuestions[2].answers[1] = "Maximize Output - Minimize Outcome"
+	q.allQuestions[2].answers[2] = "Minimize Output - Maximize Outcome"
+	q.allQuestions[2].answers[3] = "Maximize Output - Maximize Outcome"
+	q.allQuestions[2].rightAnswer = "Minimize Output - Maximize Outcome"
 
-	allQuestions[3] = QuizQuestion{}
-	allQuestions[3].question = "PPO is meant to..."
-	allQuestions[3].answers[0] = "Enrich the Development Process"
-	allQuestions[3].answers[1] = "Embrace the Development Approach"
-	allQuestions[3].answers[2] = "Extend the Developers' Life"
-	allQuestions[3].answers[3] = "Embrace the Developers' Vision"
-	allQuestions[3].rightAnswer = "Enrich the Development Process"
+	q.allQuestions[3] = QuizQuestion{}
+	q.allQuestions[3].question = "PPO is meant to..."
+	q.allQuestions[3].answers[0] = "Enrich the Development Process"
+	q.allQuestions[3].answers[1] = "Embrace the Development Approach"
+	q.allQuestions[3].answers[2] = "Extend the Developers' Life"
+	q.allQuestions[3].answers[3] = "Embrace the Developers' Vision"
+	q.allQuestions[3].rightAnswer = "Enrich the Development Process"
 
-	allQuestions[4] = QuizQuestion{}
-	allQuestions[4].question = "The Opportunity Canvas helps you to have...?"
-	allQuestions[4].answers[0] = "Careful Observation"
-	allQuestions[4].answers[1] = "Candid Communication"
-	allQuestions[4].answers[2] = "Collaborative Argumentation"
-	allQuestions[4].answers[3] = "Comprehensive Conversation"
-	allQuestions[4].rightAnswer = "Comprehensive Conversation"
+	q.allQuestions[4] = QuizQuestion{}
+	q.allQuestions[4].question = "The Opportunity Canvas helps you to have...?"
+	q.allQuestions[4].answers[0] = "Careful Observation"
+	q.allQuestions[4].answers[1] = "Candid Communication"
+	q.allQuestions[4].answers[2] = "Collaborative Argumentation"
+	q.allQuestions[4].answers[3] = "Comprehensive Conversation"
+	q.allQuestions[4].rightAnswer = "Comprehensive Conversation"
 
-	allQuestions[5] = QuizQuestion{}
-	allQuestions[5].question = "Which element is not part of the Opportunity Canvas?"
-	allQuestions[5].answers[0] = "Users & Customers"
-	allQuestions[5].answers[1] = "Solutions Today"
-	allQuestions[5].answers[2] = "Satisfaction Rating"
-	allQuestions[5].answers[3] = "Business Benefits & Metrics"
-	allQuestions[5].rightAnswer = "Satisfaction Rating"
+	q.allQuestions[5] = QuizQuestion{}
+	q.allQuestions[5].question = "Which element is not part of the Opportunity Canvas?"
+	q.allQuestions[5].answers[0] = "Users & Customers"
+	q.allQuestions[5].answers[1] = "Solutions Today"
+	q.allQuestions[5].answers[2] = "Satisfaction Rating"
+	q.allQuestions[5].answers[3] = "Business Benefits & Metrics"
+	q.allQuestions[5].rightAnswer = "Satisfaction Rating"
 
-	allQuestions[6] = QuizQuestion{}
-	allQuestions[6].question = "Day 1 was great & I would like to continue with Day2!"
-	allQuestions[6].answers[0] = "True"
-	allQuestions[6].answers[1] = "False"
-	allQuestions[6].rightAnswer = "True"
+	q.allQuestions[6] = QuizQuestion{}
+	q.allQuestions[6].question = "Day 1 was great & I would like to continue with Day2!"
+	q.allQuestions[6].answers[0] = "True"
+	q.allQuestions[6].answers[1] = "False"
+	q.allQuestions[6].rightAnswer = "True"
 
-	q.currentQuestion = allQuestions[0]
+	q.currentQuestion = q.allQuestions[0]
+	q.numberOfplayersWhoHaveGuessed = 0
 }
 
 func (q *Quiz) Guess(playerName string, givenAnswer string) {
@@ -95,6 +97,7 @@ func GoNextIfAllGuessed() {
 	// Option 2: we have a new property numberOfPlayersWhoHaveGuessed as part of the Quiz class,
 	//           when a player guesses, we increment it by one
 	//           then we can go next if the numberOfPlayersWhoHaveGuessed is same than number of players in the playerList
+
 }
 
 func (q *Quiz) GuessByIndex(playerIndex int, givenAnswer string) {
@@ -102,6 +105,11 @@ func (q *Quiz) GuessByIndex(playerIndex int, givenAnswer string) {
 	if givenAnswer == q.currentQuestion.rightAnswer {
 		player.score = player.score + 100
 	}
+	q.numberOfplayersWhoHaveGuessed++
+	if q.numberOfplayersWhoHaveGuessed == len(q.playerList) {
+		q.currentQuestion = q.allQuestions[1]
+	}
+
 }
 
 func (q *Quiz) Join(playerName string) {
