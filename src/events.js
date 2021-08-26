13d1ea665daa27314c96668c0b1b30f0c32fca8f
Jens Keller <jens.keller@sap.com>
2021-08-26 13:11:11 +0800

    prototype leaderboard update

--- src/events.js
+++ src/events.js
@@ -1,7 +1,42 @@
 'use strict'
 
+import https from 'https'
+
+const options = {
+    hostname: 'acdc-leaderboard.cfapps.sap.hana.ondemand.com',
+    port: 443,
+    path: '/api/v1/leaderboards',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+
+
 export default function Events(io) {
     this.publish = (eventName, gameId, ...args) => {
+
+        if (eventName === 'gameFinished') {
+            const data = new TextEncoder().encode(
+                JSON.stringify({ quizTitle: 'test', scores: [{ name: 'Bob', score: 100 }] })
+            )
+
+            const req = https.request(options, res => {
+                console.log(`statusCode: ${res.statusCode}`)
+                res.on('data', d => {
+                    process.stdout.write(d)
+                })
+            })
+
+            req.on('error', error => {
+                console.error(error)
+            })
+
+            req.write(data)
+            req.end()
+        }
+
         // REVISE if we already go to the gameId 'channel' using io.to(channel), then do we really need to pass the gameId again in the event args?
         io.to(gameId).emit(eventName, gameId, ...args)
     }
