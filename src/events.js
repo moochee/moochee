726c74869f16f552fcff04c723d77782fd18f94c
Yang Zhao <yang.zhao@sap.com>
2021-08-26 17:06:04 -0700

    use env var to control if update leaderboard or not

--- src/events.js
+++ src/events.js
@@ -1,43 +1,12 @@
 'use strict'
 
-import https from 'https'
-
-const options = {
-    hostname: 'acdc-leaderboard.cfapps.sap.hana.ondemand.com',
-    port: 443,
-    path: '/api/v1/leaderboards',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
-
-
+import update from './leaderboard-updater.js'
 
 export default function Events(io) {
     this.publish = (eventName, gameId, ...args) => {
-
-        if (eventName === 'gameFinished') {
-            const data = new TextEncoder().encode(
-                JSON.stringify({ quizTitle: 'test', scores: [{ name: 'Bob', score: 100 }] })
-            )
-
-            const req = https.request(options, res => {
-                console.log(`statusCode: ${res.statusCode}`)
-                res.on('data', d => {
-                    process.stdout.write(d)
-                })
-            })
-
-            req.on('error', error => {
-                console.error(error)
-            })
-
-            req.write(data)
-            req.end()
-        }
-
         // REVISE if we already go to the gameId 'channel' using io.to(channel), then do we really need to pass the gameId again in the event args?
         io.to(gameId).emit(eventName, gameId, ...args)
+
+        process.env.UPDATE_LEADERBOARD ? update(eventName, args[0]) : null
     }
 }
---
+++ src/leaderboard-updater.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import https from 'https'
+
+const options = {
+    hostname: 'acdc-leaderboard.internal.cfapps.sap.hana.ondemand.com',
+    port: 443,
+    path: '/api/v1/leaderboards',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}
+
+export default function update(eventName, result) {
+    if (eventName === 'gameFinished') {
+        const scores = result.map(({ name, score }) => ({ name, score }))
+        const data = new TextEncoder().encode(
+            JSON.stringify({ scores: scores })
+        )
+
+        const req = https.request(options, res => {
+            console.log(`statusCode: ${res.statusCode}`)
+        })
+
+        req.on('error', error => {
+            console.error(error)
+        })
+
+        req.write(data)
+        req.end()
+    }
+}
