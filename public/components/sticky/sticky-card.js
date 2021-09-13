fd5b162077caaa0339a73ff8a873b19683dbb557
Jens Keller <jens.keller@sap.com>
2021-09-13 15:52:43 +0800

    have numbers for distribution on lower right of card

--- public/components/sticky/sticky-card.js
+++ public/components/sticky/sticky-card.js
@@ -6,14 +6,8 @@ import loadCss from '/load-css.js'
 loadCss('/components/sticky/sticky-card.css')
 
 export default function StickyCard(props) {
-    const countBlock = props.count != undefined ? html`<div class=count>${props.count}</div>` : ''
-    const cssClasses = props.wrong ? 'stickyCard wrongAnswer' : 'stickyCard rightAnswer'
-
-    return html`<div>
-        <div class=${cssClasses}>
-            <img src=components/sticky/sticky-${props.color}.svg></img>
-            <div class=textField>${props.text}</div>
-        </div>
-        ${countBlock}
+    return html`<div class=stickyCard>
+        <img src=components/sticky/sticky-${props.color}.svg></img>
+        <div class=textField>${props.text}</div>
     </div>`
 }
