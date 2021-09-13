82bbdc9c1d9be7c020bbb770d4331fb4f9105899
Yang Zhao <yang.zhao@sap.com>
2021-09-13 15:30:18 -0700

    dark background (wip)

--- public/components/distribution/distribution.js
+++ public/components/distribution/distribution.js
@@ -11,8 +11,9 @@ export default function Distribution(props) {
     const colors = ['green', 'purple', 'blue', 'orange']
 
     const answersBlock = props.distribution.answers.map((answer, index) => {
-        const style = (answer.id === props.distribution.rightAnswerId) ? '' : 'opacity: 40%; transform: scale(.8);'
-        const countStyle ='position: relative; bottom: 30%; right: 15%; font-size: min(5vw, 5vh); color: white; text-align: right;'
+        const style = (answer.id === props.distribution.rightAnswerId) ? '' : 'opacity: 60%; transform: scale(.8);'
+        // const text = (answer.id === props.distribution.rightAnswerId) ?
+        const countStyle = 'position: relative; bottom: 30%; right: 15%; font-size: min(5vw, 5vh); color: white; text-align: right;'
         return html`<div style=${style}>
             <${StickyCard} key=${index} color=${colors[index]} text=${answer.text} info=${answer.count}/>
             <div style=${countStyle}>${answer.count}</div>
