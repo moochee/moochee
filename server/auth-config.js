b55ba845da741d0f42020415b38f653152745bda
Yang Zhao <yang.zhao@sap.com>
2021-10-14 13:47:58 -0700

    use vcap_application to avoid hardcoding redirect uri

--- server/auth-config.js
+++ server/auth-config.js
@@ -3,11 +3,14 @@
 const vcapServices = JSON.parse(process.env.VCAP_SERVICES || {})
 const serviceEnvs = Object.keys(vcapServices).map(key => vcapServices[key]).flat()
 const uaa = serviceEnvs.find(entry => entry.name === 'gorilla-uaa')
+const vcapApplication = JSON.parse(process.env.VCAP_APPLICATION || {})
+const uri = vcapApplication.uris[0]
+const protocol = uri.indexOf('localhost') >= 0 ? 'http' : 'https'
 
 export default {
     IDP: uaa.credentials.url,
     CLIENT_ID: uaa.credentials.clientid,
     CLIENT_SECRET: uaa.credentials.clientsecret,
-    REDIRECT_URI: process.env.REDIRECT_URI || 'https://acdc-gorilla.cfapps.sap.hana.ondemand.com/login/callback',
+    REDIRECT_URI: `${protocol}://${uri}/login/callback`,
     SESSION_SECRET: process.env.SESSION_SECRET || uaa.credentials.clientsecret
 }
