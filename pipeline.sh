4b63d5736b1b99636ec47b4f1989316944295e11
Jens Keller <jens.keller@sap.com>
2021-11-11 17:41:39 +0800

    pipeline script

---
+++ pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "there are uncommitted chages, stopping pipeline"
+    exit 1
+fi
+
+git fetch
+
+if [ "$(git rev-parse HEAD)" = "$(git rev-parse origin/master)" ]; then
+    echo "current branch is not in sync with origin/master, stopping pipeline"
+    exit 1
+fi
+
+cf target -s acdc-gorilla-test
+
+cf target -s acdc-gorilla
