a17e2a0a0724745bfa44188f2b9001d1d0279314
Jens Keller <jens.keller@sap.com>
2021-12-21 16:43:31 +0800

    use DEDICATED_ORIGIN instead of ambiguous application uri

--- server/auth-config.js
+++ server/auth-config.js
@@ -1,20 +1,16 @@
 'use strict'
 
-export default function getAuthConfig(vcapServices = process.env.VCAP_SERVICES, vcapApplication = process.env.VCAP_APPLICATION) {
-    const services = JSON.parse(vcapServices || {})
+export default function getAuthConfig() {
+    const services = JSON.parse(process.env.VCAP_SERVICES)
     const serviceEnvs = Object.keys(services).map(key => services[key]).flat()
     const uaa = serviceEnvs.find(entry => entry.name === 'gorilla-uaa')
-
-    const application = JSON.parse(vcapApplication || {})
-    // fallback uri is provided because url in VCAP_APPLICATION is empty when deploying new version hidden
-    const uri = application.uris[0] || 'acdc-gorilla.cfapps.sap.hana.ondemand.com'
-    const protocol = uri.indexOf('localhost') >= 0 ? 'http' : 'https'
+    const host = process.env.DEDICATED_ORIGIN
 
     return {
         IDP: uaa.credentials.url,
         CLIENT_ID: uaa.credentials.clientid,
         CLIENT_SECRET: uaa.credentials.clientsecret,
-        REDIRECT_URI: `${protocol}://${uri}/login/callback`,
+        REDIRECT_URI: `${host}/login/callback`,
         SESSION_SECRET: process.env.SESSION_SECRET || uaa.credentials.clientsecret
     }
 }
