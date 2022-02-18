02336d8345dc692d192c726aaefa96411fffa824
Jens Keller <jens.keller@sap.com>
2022-02-18 13:56:57 +0800

    simplified graceful shutdown test, fixed edge case

--- graceful-shutdown-test.sh
+++ graceful-shutdown-test.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -e
 
-echoerr() { >&2 echo $@; }
-
-echo 'starting server'
-
+# REVISE get rid of local profile, e.g. use some no-auth approach or dummy uaa data
 source local.profile
 node server/gorilla.js ./local-client.js &
 pid=$!
@@ -12,32 +9,17 @@ attempts=0
 until $(curl --output /dev/null --silent --fail http://localhost:3000/api/v1/status); do
     attempts=$((attempts+1))
     if [ "$attempts" == "10" ]; then
-        echoerr 'app not started after 10 attempts, giving up'
+        echo 'app not started after 10 attempts, giving up'
         exit 1
     fi
     sleep 1
 done
 
-echo 'server started, requesting to stop'
+echo "server started (process $pid), requesting to stop"
 
 curl --fail -X POST http://localhost:3000/api/v1/stop
 
-attempts=0
-while $(curl --output /dev/null --silent --fail http://localhost:3000/api/v1/status); do
-    attempts=$((attempts+1))
-    if [ "$attempts" == "10" ]; then
-        echoerr 'ERROR: app not stopped after 10 attempts, giving up'
-        kill $pid
-        exit 1
-    fi
-    sleep 1
-done
+(sleep 5; if ps -p $pid > /dev/null; then echo 'timeout'; kill -9 $pid; fi;) &
+timeoutPid=$!
 
-echo 'server stopped'
-
-if ps -p $pid > /dev/null; then
-   echoerr "application did not completely terminate, process still exist: $pid"
-   echoerr 'killing process now to prevent stale processes piling up, fix this immediately'
-   kill $pid
-   exit 1
-fi
+wait $pid
--- server/local-client.js
+++ server/local-client.js
@@ -3,8 +3,15 @@ import { exec } from 'child_process'
 export default function create() {
     return {
         stop: () => {
-            // REVISE check if this causes trouble since it is not returning a promise
-            exec(`kill ${process.pid}`)
+            return new Promise((resolve, reject) => {
+                exec(`kill ${process.pid}`, (error) => {
+                    if (error) {
+                        reject(error)
+                    } else {
+                        resolve()
+                    }
+                })
+            })
         }
     }
 }
