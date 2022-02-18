6336aa4f64db1eb1a7ed4c78cead0d8ecdf825b3
Jens Keller <jens.keller@sap.com>
2022-02-18 14:32:08 +0800

    new scoreboard logic

--- web/public/components/scoreboard/scoreboard.js
+++ web/public/components/scoreboard/scoreboard.js
@@ -5,48 +5,52 @@ import loadCss from '/public/load-css.js'
 
 loadCss('/public/components/scoreboard/scoreboard.css')
 
-// TODO move remaining styling to css
+// REVISE move remaining styling to css
 
 export default function Scoreboard(props) {
     // TODO tryout counter animation using css @property https://css-tricks.com/animating-number-counters/
-    const rankingClasses = {}
 
-    props.ranking.forEach(entry => {
-        if (entry.oldRank <= 5 && entry.rank > 5) {
-            rankingClasses[entry.avatar] = 'rankingMoveOut'
-        } else if (entry.oldRank > 5 && entry.rank <= 5) {
-            rankingClasses[entry.avatar] = 'rankingMoveIn'
-        } else if (entry.oldRank <= 5 && entry.rank <= 5) {
-            rankingClasses[entry.avatar] = 'rankingMove'
-        } else if (!entry.oldRank && entry.rank <= 5) {
-            rankingClasses[entry.avatar] = 'rankingAppear'
+    // REVISE still some spaghetti - this works because the scoreboard is sorted by oldRank elsewhere
+    const top5oldAndNew = props.scoreboard.filter(entry => {
+        return (entry.rank <= 5 || entry.oldRank <= 5)
+    }).map((entry, index) => ({ ...entry, oldRank: index + 1 }))
+
+    const entries = JSON.parse(JSON.stringify(top5oldAndNew)).map(entry => {
+        // REVISE calculate shift here and then maybe can simplify/unify the CSS classes
+        if (entry.oldRank > 5) {
+            return { ...entry, class: 'scoreboardMoveIn' }
+        } else if (entry.rank > 5) {
+            return { ...entry, class: 'scoreboardMoveOut', rank: 6 }
         } else {
-            rankingClasses[entry.avatar] = 'rankingInvisible'
+            return { ...entry, class: 'scoreboardMove' }
         }
     })
 
-    const entries = props.ranking.map(e => {
-        return html`<div key=${e.avatar} class=${rankingClasses[e.avatar]} style='display: flex; align-items: center; --positions: ${e.rank - e.oldRank}'>
+    const entriesHtml = entries.map(e => {
+        return html`<div key=${e.avatar} class=${e.class} style='display: flex; align-items: center; --positions: ${e.rank - e.oldRank}'>
             <div style='margin-right: 0.5em; font-size: 2em;'>${e.avatar}</div>
             <div style='white-space: nowrap;'>${e.score} points</div>
         </div>`
     })
 
     const scoreboard = useRef()
+
     const setDimensions = () => {
         const height = Math.min(window.innerHeight, window.innerWidth * 9 / 16) / 100
         scoreboard.current.style.setProperty('--height', height)
         scoreboard.current.style.setProperty('--width', height * 16 / 9)
     }
+
     useEffect(() => {
         setDimensions()
         window.addEventListener('resize', setDimensions)
         return () => window.removeEventListener('resize', setDimensions)
     }, [])
 
-    return html`<div ref=${scoreboard} class=ranking>
-        <div style='display: flex; flex-direction: column; align-items: center; margin-top: 12vh;'>
-            ${entries}
+    // REVISE check if we need the nested DIVs
+    return html`<div ref=${scoreboard} class=scoreboard>
+        <div style='display: flex; flex-direction: column; align-items: center;'>
+            ${entriesHtml}
         </div>
     </div>`
 }
