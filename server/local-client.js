a749dfb86312e5195f00555b3d50bcd49b5b6953
Jens Keller <jens.keller@sap.com>
2022-02-18 10:49:22 +0800

    revise

--- server/local-client.js
+++ server/local-client.js
@@ -3,6 +3,7 @@ import { exec } from 'child_process'
 export default function create() {
     return {
         stop: () => {
+            // REVISE check if this causes trouble since it is not returning a promise
             exec(`kill ${process.pid}`)
         }
     }
