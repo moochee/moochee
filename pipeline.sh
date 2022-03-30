b13ca82d8fc53ab633da5158994cfa6c51603b4e
Jens Keller <jens.keller@sap.com>
2022-03-30 23:19:22 +0800

    login in pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -8,9 +8,11 @@ npm run lint
 npm test
 ./graceful-shutdown-test.sh
 
+cf login -a https://api.cf.sap.hana.ondemand.com -u $CF_USER -p $CF_PW -o cc-acdc-tools
+
 echo 'Integration stage'
 cf target -s gorilla-quiz-test
-./deploy.sh dummy-cf-user dummy-cf-pw -test #TODO use CF tech user and supply creds through env
+./deploy.sh $CF_USER $CF_PW -test #TODO use CF tech user and supply creds through env
 
 #echo 'Production stage'
 #cf target -s gorilla-quiz
