24c60fd6dd60ec15efccbee8d7fa9fb4b9d3bde0
Jens Keller <jens.keller@sap.com>
2022-03-31 00:18:10 +0800

    enable pipeline for prod

--- pipeline.sh
+++ pipeline.sh
@@ -12,8 +12,8 @@ cf login -a https://api.cf.sap.hana.ondemand.com -u $CF_USER -p $CF_PW -o cc-acd
 
 echo 'Integration stage'
 cf target -s gorilla-quiz-test
-./deploy.sh $CF_USER $CF_PW -test #TODO use CF tech user and supply creds through env
+./deploy.sh $CF_USER $CF_PW -test
 
-#echo 'Production stage'
-#cf target -s gorilla-quiz
-#./deploy.sh dummy-cf-user dummy-cf-pw #TODO use CF tech user and supply creds through env
+echo 'Production stage'
+cf target -s gorilla-quiz
+./deploy.sh $CF_USER $CF_PW
