83ee7255beb85f52727203ce2fae16bfa4324493
Elina <elina.cao@sap.com>
2022-04-08 17:34:25 +0800

    show tooltip for tags

--- web/public/components/sticky/sticky-button.js
+++ web/public/components/sticky/sticky-button.js
@@ -7,28 +7,26 @@ loadCss('/public/components/sticky/sticky-button.css')
 
 export default function StickyButton(props) {
     const [stickyClass, setStickyClass] = useState('stickyButton')
-    const [showMore, setShowMore] = useState(false)
 
     const click = () => {
         setStickyClass('stickyButton stickyThrowAway')
     }
 
     const tags = props.tags
-        .map(tag => html`<div class='tag ${props.color}'>${tag}</div>`)
+        .map(tag => html`<div class='tag ${props.color}' title=${tag}>${tag}</div>`)
+        .slice(0, 4)
 
-    const more = showMore ? html`<div>more tags here</div>` : '...'
-    if (props.tags.length > 2) {
-        tags.push(html`<div class='tag ${props.color}'>${more}</div>`)
+    if (props.tags.length > 4) {
+        const tooltip = props.tags.slice(4).join(', ')
+        tags.push(html`<div class='tag ${props.color}' title=${tooltip}>...</div>`)
     }
-    // ${showMore ? tags : tags.slice(0, 2)}
-    // onMouseLeave=${setShowMore(false)}
-    
+
     return html`<div onAnimationEnd=${props.onClick} onClick=${click} class=${stickyClass}>
         <img src=/public/components/sticky/sticky-${props.color}.svg></img>
         <div class=textField>
             ${props.text}
         </div>
-        <div class=tags onMouseEnter=${setShowMore(true)}>
+        <div class=tags>
             ${tags}
         </div>
     </div>`
