70a61d46c3ba59268613b170af9cef0b92703b5d
Jens Keller <jens.keller@sap.com>
2022-04-10 14:41:48 +0800

    delete unused files

--- server/fs-directory.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-export default function getFSDirectory(vcapServices = process.env.VCAP_SERVICES) {
-    const services = JSON.parse(vcapServices || {})
-    const dir = services['fs-storage'].find(f => f.name === 'gorilla-fs').volume_mounts[0].container_dir
-    return dir
-}
