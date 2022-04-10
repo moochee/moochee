f5f3e2adec6ad6465544050fc9980e28a0d5d211
Jens Keller <jens.keller@sap.com>
2022-04-10 15:43:44 +0800

    use top level await

--- server/gorilla.js
+++ server/gorilla.js
@@ -2,15 +2,10 @@
 
 import httpServer from './http-server.js'
 
-// REVISE check if with newer Node.js version we can use top-level await
-const init = async () => {
-    // REVISE maybe this could also be moved to the bindings
-    const gameExpiryTimer = {
-        onTimeout: (callback) => setTimeout(callback, 1000 * 60 * 60 * 3)
-    }
-    const { appStopper, auth, privateQuizzesDir, dedicatedOrigin, port } = (await import(process.argv[2])).default
-    const server = httpServer(appStopper, auth, privateQuizzesDir, dedicatedOrigin, gameExpiryTimer)
-    server.listen(port, () => console.log(`Gorilla started at ${port}`))
+// REVISE maybe this could also be moved to the bindings
+const gameExpiryTimer = {
+    onTimeout: (callback) => setTimeout(callback, 1000 * 60 * 60 * 3)
 }
-
-init()
+const { appStopper, auth, privateQuizzesDir, dedicatedOrigin, port } = (await import(process.argv[2])).default
+const server = httpServer(appStopper, auth, privateQuizzesDir, dedicatedOrigin, gameExpiryTimer)
+server.listen(port, () => console.log(`Gorilla started at ${port}`))
