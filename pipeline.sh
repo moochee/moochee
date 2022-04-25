25dc84823d20d47fb131c7a55b2e5774995914e3
Jens Keller <jens.keller@sap.com>
2022-04-25 15:50:39 +0800

    whitesource

--- pipeline.sh
+++ pipeline.sh
@@ -7,11 +7,12 @@ npm ci
 npm run lint
 npm test
 ./graceful-shutdown-test.sh
-
-cf api https://api.cf.sap.hana.ondemand.com
-cf auth $CF_USER $CF_PW
+curl -O --silent https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar
+WS_WSS_URL=https://sap.whitesourcesoftware.com/agent WS_FORCECHECKALLDEPENDENCIES=true WS_FORCEUPDATE=true WS_FORCECHECKPOLICIES=true WS_FAILERRORLEVEL=all java -jar wss-unified-agent.jar
 
 echo 'Integration stage'
+cf api https://api.cf.sap.hana.ondemand.com
+cf auth $CF_USER $CF_PW
 cf target -o cc-acdc-tools -s gorilla-quiz-test
 ./deploy.sh $CF_USER $CF_PW -test
 
