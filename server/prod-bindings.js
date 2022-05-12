81d1f1df777e3137ea317fc1c8abd2c6361f4e72
Jens Keller <jens.keller@sap.com>
2022-05-12 20:46:22 +0800

    get API endpoint from env

--- server/prod-bindings.js
+++ server/prod-bindings.js
@@ -6,6 +6,7 @@ import Auth from './auth.js'
 const dedicatedOrigin = process.env.DEDICATED_ORIGIN
 
 const services = JSON.parse(process.env.VCAP_SERVICES)
+const app = JSON.parse(process.env.VCAP_APPLICATION)
 const serviceEnvs = Object.keys(services).map(key => services[key]).flat()
 
 const uaa = serviceEnvs.find(entry => entry.name === 'gorilla-uaa')
@@ -23,7 +24,7 @@ const cfUser = process.env['CF_USER'], cfPw = process.env['CF_PW']
 
 export default {
     auth: new Auth(authConfig),
-    appStopper: new CFClient('https://api.cf.sap.hana.ondemand.com', cfUser, cfPw),
+    appStopper: new CFClient(app.cf_api, cfUser, cfPw),
     privateQuizzesDir: privateQuizzesDir,
     dedicatedOrigin: dedicatedOrigin,
     port: process.env.PORT
