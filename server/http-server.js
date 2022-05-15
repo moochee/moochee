aedc76aa2d0110b95dade0a50a766640f821a1c0
Jens Keller <jens.keller@sap.com>
2022-05-15 20:19:57 +0800

    server htm+preact from node modules

--- server/http-server.js
+++ server/http-server.js
@@ -35,6 +35,7 @@ export default function create(client, auth, directory, dedicatedOrigin, gameExp
 
     app.use('/public', express.static('web/public'))
     app.use('/play', express.static('web/play'))
+    app.use('/lib/htm/preact/standalone.module.js', express.static('./node_modules/htm/preact/standalone.module.js'))
 
     app.use(express.json())
     app.use('/api/v1/quizzes', quizRouter(directory))
