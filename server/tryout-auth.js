00eea94aca73781e98eeab63d69a2cfacee0d6a7
Yang Zhao <yang.zhao@sap.com>
2022-06-05 07:21:46 -0700

    only tryout can bypass auth

--- server/tryout-auth.js
+++ server/tryout-auth.js
@@ -3,7 +3,9 @@
 export default function TryoutAuth() {
     this.setup = () => {
         return (req, res, next) => {
-            if (!req.user) {
+            const referer = req.get('Referer')
+            console.log(referer)
+            if (referer?.indexOf('tryout') > -1 && !req.user) {
                 req.isAuthenticated = () => true
                 req.user = { id: 'john.doe@acme.org' }
             }
