8cc00564d5bdb25536fca6e4095bc8aaa8ba87b5
Yang Zhao <yang.zhao@sap.com>
2022-06-05 18:59:15 -0700

    remove console.log

--- server/tryout-auth.js
+++ server/tryout-auth.js
@@ -4,7 +4,6 @@ export default function TryoutAuth() {
     this.setup = () => {
         return (req, res, next) => {
             const referer = req.get('Referer')
-            console.log(referer)
             if (referer?.indexOf('tryout') > -1 && !req.user) {
                 req.isAuthenticated = () => true
                 req.user = { id: 'john.doe@acme.org' }
