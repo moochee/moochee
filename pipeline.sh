9277bf50c10e993e26bdbadc70dc15c68f8d0906
Yang Zhao <yang.zhao@sap.com>
2022-06-13 21:05:53 -0700

    work on pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,21 +1,11 @@
 #!/bin/sh -e
 
-./assert-clean-local-repo.sh
-
 echo 'Local stage'
 npm ci
 npm run lint
 npm test
 ./graceful-shutdown-test.sh
-curl -O --silent https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar
-WS_WSS_URL=https://sap.whitesourcesoftware.com/agent WS_FORCECHECKALLDEPENDENCIES=true WS_FORCEUPDATE=true WS_FORCECHECKPOLICIES=true WS_FAILERRORLEVEL=all java -jar wss-unified-agent.jar
 
-echo 'Integration stage'
-cf api https://api.cf.eu12.hana.ondemand.com
-cf auth $CF_USER $CF_PW
-cf target -o additional-services-xrnbvs9d -s test.service.gorilla-quizz
-./deploy.sh $CF_USER $CF_PW -test
-
-echo 'Production stage'
-cf target -o additional-services-xrnbvs9d -s service.gorilla-quizz
-./deploy.sh $CF_USER $CF_PW
+# echo 'Production stage'
+# cf target -o additional-services-xrnbvs9d -s service.gorilla-quizz
+# ./deploy.sh $CF_USER $CF_PW
