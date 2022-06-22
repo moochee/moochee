80d8110db5159c802416ea273c025b99b03cfff0
Yang Zhao <yang.zhao@sap.com>
2022-06-22 00:18:00 -0700

    pipeline - wip

--- pipeline.sh
+++ pipeline.sh
@@ -6,6 +6,5 @@ npm run lint
 npm test
 ./graceful-shutdown-test.sh
 
-# echo 'Production stage'
-# cf target -o additional-services-xrnbvs9d -s service.gorilla-quizz
-# ./deploy.sh $CF_USER $CF_PW
+echo 'Production stage'
+./deploy.sh
