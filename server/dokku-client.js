b6021ec46e3c3b2b6756ee077ca0d9d19f77cc5d
Yang Zhao <yang.zhao@sap.com>
2022-06-23 13:39:41 -0700

    specify cwd & shell when execute cmd in node

--- server/dokku-client.js
+++ server/dokku-client.js
@@ -6,7 +6,7 @@ export default function DokkuClient() {
     this.stop = () => {
         const appName = process.env.APP_NAME
         return new Promise((resolve, reject) => {
-            exec(`/app/bin/dokku ps:stop ${appName}`, (error) => {
+            exec(`dokku ps:stop ${appName}`, { cwd: '/app/bin', shell: '/app/bin/sh'}, (error) => {
                 return error ? reject(error) : resolve()
             })
         })
