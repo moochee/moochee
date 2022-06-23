112aca389a60a9827788afd6cd6127ce4213da99
Yang Zhao <yang.zhao@sap.com>
2022-06-23 14:01:41 -0700

    fix exec by providing path for dokku

--- server/dokku-client.js
+++ server/dokku-client.js
@@ -6,7 +6,7 @@ export default function DokkuClient() {
     this.stop = () => {
         const appName = process.env.APP_NAME
         return new Promise((resolve, reject) => {
-            exec(`dokku ps:stop ${appName}`, { cwd: '/app/bin', shell: '/app/bin/sh'}, (error) => {
+            exec(`/app/bin/dokku ps:stop ${appName}`, { shell: '/app/bin/sh'}, (error) => {
                 return error ? reject(error) : resolve()
             })
         })
