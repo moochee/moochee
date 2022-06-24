daa779c4283e580266f51a512c718b69b014eceb
Yang Zhao <yang.zhao@sap.com>
2022-06-24 10:36:23 -0700

    user docker as app stopper

--- server/dokku-client.js
+++ server/dokku-client.js
@@ -6,7 +6,7 @@ export default function DokkuClient() {
     this.stop = () => {
         const appName = process.env.APP_NAME
         return new Promise((resolve, reject) => {
-            exec(`/app/bin/dokku ps:stop ${appName}`, (error) => {
+            exec(`docker run --privileged --pid=host -it node:14-alpine nsenter -t 1 -m -u -n -i bash dokku ps:stop ${appName}`, (error) => {
                 return error ? reject(error) : resolve()
             })
         })
