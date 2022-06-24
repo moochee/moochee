6f47608fd885a28dc4c17637bb1a8a2b951cb29d
Yang Zhao <yang.zhao@sap.com>
2022-06-24 10:15:39 -0700

    install bash in docker image

--- server/dokku-client.js
+++ server/dokku-client.js
@@ -6,7 +6,7 @@ export default function DokkuClient() {
     this.stop = () => {
         const appName = process.env.APP_NAME
         return new Promise((resolve, reject) => {
-            exec(`/app/bin/dokku ps:stop ${appName}`, { shell: '/app/bin/sh'}, (error) => {
+            exec(`/app/bin/dokku ps:stop ${appName}`, (error) => {
                 return error ? reject(error) : resolve()
             })
         })
