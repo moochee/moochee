83fb2f48be738f9ab856e0fe8cf1c3c3231ce2a5
Yang Zhao <yang.zhao@sap.com>
2022-06-24 14:49:16 -0700

    add env var

--- server/dokku-client.js
+++ server/dokku-client.js
@@ -6,7 +6,10 @@ export default function DokkuClient() {
     this.stop = () => {
         const appName = process.env.APP_NAME
         return new Promise((resolve, reject) => {
-            exec(`/host/usr/bin/dokku ps:stop ${appName}`, { shell: 'bash'}, (error) => {
+            exec(`/host/usr/bin/dokku ps:stop ${appName}`, { 
+                shell: 'bash',
+                env: {'DOKKU_LIB_PATH': '/host/var/lib/dokku'}
+            }, (error) => {
                 return error ? reject(error) : resolve()
             })
         })
